e5ffab17e315b5d50e0dfda0501173dac3ae7d74
Tellow Krinkle <etang110@gmail.com>
2014-02-14 18:12:53 -0600

    Better Comments!
    I described everything to make the code easier to understand.

--- src/2014Robot.cpp
+++ src/2014Robot.cpp
@@ -76,30 +76,30 @@ public:
 		driverStation = DriverStation::GetInstance();
 	}
 	void Autonomous(void) {
-		motorRight->Set(0.7);
+		motorRight->Set(-0.7);
 		motorLeft->Set(0.7);
 		Wait(1.5);
 		motorRight->Set(0.0);
 		motorLeft->Set(0.0);
 	}
 	void OperatorControl(void) {
-		bool shifting = false;
-		int pendingShift = 0;
-		double shiftTimer = 0.0;
-		double lowFreqTimer = 0.0;
-		double shooterTimeout = 0.0;
-		float shooterFarShot = 0.0;
-		float shooterCloseShot = 0.0;
-		float shooterBottom = 0.0;
+		bool shifting = false; //Variable for shifting
+		int pendingShift = 0; //Stores what if any shifts are pending.  1.0 = shift high, 0.0 = none, -1.0 = shift low
+		double shiftTimer = 0.0; //The timeout will set the pending shift to 0.0 if the timer passes this time.
+		double lowFreqTimer = 0.0; //Stores the next time that the low frequency code runs
+		double shooterTimeout = 0.0; //Backup timeout for shooter in case the sensor malfunctions
+		float shooterFarShot = 0.0; //Potentiometer reading at top of a far shot
+		float shooterCloseShot = 0.0; //Potentiometer reading at top of a close shot
+		float shooterBottom = 0.0; //Potentiometer reading when shooter arm is at the bottom spot
 		while (IsOperatorControl()) {  //We only want this to run while in teleop mode!
 			// Make the robot drive!
-			if(fabs(joystickLeft->GetY()) > 0.05) {
-				motorLeft->Set(joystickLeft->GetY() * fabs(joystickLeft->GetY()));
+			if(fabs(joystickLeft->GetY()) > 0.05) { //Only run motors if the joystick is past 5% from middle
+				motorLeft->Set(joystickLeft->GetY() * fabs(joystickLeft->GetY())); //Set the left motor to the square of the left joystick.
 			}
 			else {
 				motorLeft->Set(0.0);
 			}
-
+			//Same as above
 			if(fabs(joystickRight->GetY()) > 0.05) {
 				motorRight->Set(-1.0 * joystickRight->GetY() * fabs(joystickRight->GetY()));
 			}
@@ -108,27 +108,27 @@ public:
 			}
 			
 			//Lifter control
-			if(joystickRight->GetRawButton(2) && !(joystickRight->GetRawButton(3))) {
+			if(joystickRight->GetRawButton(2) && !(joystickRight->GetRawButton(3))) { //If you're pushing button 2 and not 3 on the right joystick then enable the lifter forward!
 				lifter->Set(Relay::kForward);
-				driverStationLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Loading In! ");
+				driverStationLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Loading In! "); //Print out the fact that it's happening (maybe I should send this to the netconsole...)
 			}
-			else if(joystickRight->GetRawButton(3) && !(joystickRight->GetRawButton(2))) {
+			else if(joystickRight->GetRawButton(3) && !(joystickRight->GetRawButton(2))) { //If you're pushing 3 and not 2, enable it backwards.
 				lifter->Set(Relay::kReverse);
-				driverStationLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Loading Out!");
+				driverStationLCD->Printf(DriverStationLCD::kUser_Line1, 1, "Loading Out!"); //Print out the fact that it's happening
 			}
-			else {
+			else { //If neither of the previous are true, then disable the lifter motor.
 				driverStationLCD->Printf(DriverStationLCD::kUser_Line1, 1, "            ");
 				lifter->Set(Relay::kOff);
 			}
 			
 			//Launcher control
-			if(joystickLeft->GetRawButton(6) && !(joystickLeft->GetRawButton(7))) {
-				printf("Launching!\n");
+			if(joystickLeft->GetRawButton(6) && !(joystickLeft->GetRawButton(7))) { //If you're pushing 6 and not 7 on the left joystick then launch the launcher!
+				printf("Launching!\n"); //Print out the fact that it's happening to the netconsole.
 				launcherOne->Set(1.0);
 				launcherTwo->Set(1.0);
 			}
 			else if(joystickLeft->GetRawButton(7) && !(joystickLeft->GetRawButton(6))) {
-				printf("Bringing launcher back.\n");
+				printf("Bringing launcher back.\n"); //Print out the fact that it's happening to the netconsole.
 				launcherOne->Set(-0.3);
 				launcherTwo->Set(-0.3);
 			}
@@ -138,20 +138,24 @@ public:
 			}
 			
 			//Quicklaunches
-			if (joystickLeft->GetTrigger() && joystickLeft->GetRawButton(11)) {
+			if (joystickLeft->GetTrigger() && joystickLeft->GetRawButton(11)) { //Only fire if both trigger and button are held
+				//Enable launcher motor for time specified in DriverStation analog IO
 				launcherOne->Set(1.0);
 				launcherTwo->Set(1.0);
 				Wait(driverStation->GetAnalogIn(1));
+				//Disable motors for 0.1 seconds to slow down
 				launcherOne->Set(0.0);
 				launcherTwo->Set(0.0);
 				Wait(0.1);
+				//Set motors backwards for two times the time specified in DS analog IO
 				launcherOne->Set(-0.3);
 				launcherTwo->Set(-0.3);
 				Wait(driverStation->GetAnalogIn(1)* 2);
+				//Disable motors
 				launcherOne->Set(0.0);
 				launcherTwo->Set(0.0);
 			}
-			
+			//Same thing as before, but with different buttons and different IO ports.
 			if (joystickLeft->GetTrigger() && joystickLeft->GetRawButton(10)) {
 				launcherOne->Set(1.0);
 				launcherTwo->Set(1.0);
@@ -167,24 +171,26 @@ public:
 			}
 			
 			//Gearshift control
+			//If you push the buttons, the code will put it in the pending shift variable and wait until the robot is moving to shift.
 			if (joystickRight->GetRawButton(11) && !(joystickRight->GetRawButton(10))) {
 				pendingShift = 1;
-				driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Shift High Pending");
+				driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Shift High Pending"); //Print out pending shifts!
 			}
 			else if (joystickRight->GetRawButton(10) && !(joystickRight->GetRawButton(11))) {
 				pendingShift = -1;
 				driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Shift Low Pending ");
 			}
 			
-			if (pendingShift == 1 && (fabs(motorLeft->Get()) > 0.1) && (fabs(motorRight->Get()) > 0.1)) {
-				driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Shifting High     ");
-				shiftUp->Set(true);
+			if (pendingShift == 1 && (fabs(motorLeft->Get()) > 0.1) && (fabs(motorRight->Get()) > 0.1)) { //Only enable solenoids if we're moving
+				driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Shifting High     "); //Print stuff out!  Yeah!
+				shiftUp->Set(true);  //Set Solenoids
 				shiftDown->Set(false);
 				if (!shifting) {
-					shifting = true;
+					shifting = true;  //If this is the start of a shifting operation, set the timer so shifting ends half a second later.
 					shiftTimer = timer->Get() + 0.5;
 				}
 			}
+			//Same thing as before except this is for shifting down.
 			else if (pendingShift == -1 && (fabs(motorLeft->Get()) > 0.1) && (fabs(motorRight->Get()) > 0.1)) {
 				driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "Shifting Low      ");
 				shiftUp->Set(false);
@@ -197,7 +203,7 @@ public:
 			else {
 				shiftUp->Set(false);
 				shiftDown->Set(false);
-				if (shifting) {
+				if (shifting) { //Reset shifting variable and driverstation LCD
 					driverStationLCD->Printf(DriverStationLCD::kUser_Line2, 1, "                  ");
 					shifting = false;
 				}
@@ -220,18 +226,18 @@ public:
 			}
 			
 			//Timer expirations
-			if (shifting && timer->HasPeriodPassed(shiftTimer)) {
+			if (shifting && timer->HasPeriodPassed(shiftTimer)) { //If the timer passes the time written to the shiftTimer variable, set the pending shift to none.  This will stop the shift the next run of the code
 				pendingShift = 0;
 			}
-			driverStationLCD->Printf(DriverStationLCD::kUser_Line3, 1, "Shooter: %f", shooterPot->GetVoltage());
+			driverStationLCD->Printf(DriverStationLCD::kUser_Line3, 1, "Shooter: %f", shooterPot->GetVoltage()); //Print the shooter potentiometer voltage to line 3 of the DS LCD
 			
 			//These things only run 10 times a second.  Good for network access.
 			if (timer->HasPeriodPassed(lowFreqTimer)) {
-				lowFreqTimer = lowFreqTimer + 0.1;
-				shooterFarShot = driverStation->GetAnalogIn(1);
+				lowFreqTimer = lowFreqTimer + 0.1; //Increment the timer for this stuff.
+				shooterFarShot = driverStation->GetAnalogIn(1); //Read analog inputs and set variables to them.
 				shooterCloseShot = driverStation->GetAnalogIn(2);
 				shooterBottom = driverStation->GetAnalogIn(3);
-				driverStationLCD->UpdateLCD();
+				driverStationLCD->UpdateLCD(); //Update the DS LCD with new information.
 			} 
 		}
 	}
